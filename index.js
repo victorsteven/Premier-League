ea0613c07c1c2e1962de69ad1e7ab8c3f225a8a3
steven victor <chikodi543@gmail.com>
2020-03-05 18:07:43 +0100

    Add dev docker file

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 const port = process.env.PORT || 8000;
 
-app.get('*', (req, res) => res.status(200).send({
- message: 'Welcome to this API.'
+// app.get('*', (req, res) => res.status(200).send({
+//  message: 'Welcome to this API.'
+// }));
+
+app.get('/hello', (req, res) => res.status(200).send({
+ message: 'Hello! Welcome to this API.'
 }));
 
 app.listen(port, () => {
