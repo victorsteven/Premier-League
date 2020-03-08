9c12ea4460fa52e9f40ab0c92e57ba0ec05b4d00
steven victor <chikodi543@gmail.com>
2020-03-08 12:58:01 +0100

    Refactor code to api directory

---
+++ tests/mine.test.js
@@ -0,0 +1,11 @@
+
+
+describe("UserController", function() {
+  describe("createUser", function() {
+    it("should create a user", function() {
+
+      console.log("this is the test chamber")
+
+    });
+  });
+});
