0ab78955ab93a155b81d1b0858151671e99fc171
steven victor <chikodi543@gmail.com>
2020-03-14 15:06:06 +0100

    Push to heroku

--- Dockerfile
+++ Dockerfile
@@ -17,4 +17,4 @@ COPY . .
 
 EXPOSE 8080
 
-CMD [ "dev", "index.js" ]
+CMD [ "start", "index.js" ]
--- api/database/database.js
+++ api/database/database.js
@@ -3,11 +3,11 @@ import mongoose from 'mongoose'
 //mongoose uses callback by default. but we want the javascript global promise
 mongoose.Promise = global.Promise;
 
-// var dockerConn = 'mongodb://mongo:27017/premier-league' //using docker
+// var conn = 'mongodb://mongo:27017/premier-league' //using docker
 
-const dockerConn = 'mongodb://localhost:27017/premier-league' //using docker
+const conn = 'mongodb://localhost:27017/premier-league' //using docker
 
-const mongoDB = process.env.MONGODB_URI || dockerConn;
+const mongoDB = process.env.MONGODB_URI || conn;
 
 mongoose.connect(mongoDB, {useNewUrlParser: true});
 
